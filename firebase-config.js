55fa3082e192f71e60f8f839ad29c4df37027364
igeinaoshi-prog <igeinaoshi@gmail.com>
2026-09-20 10:17:43 +0900

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDdiX3qVQl4rxQPXMdin0Zt1xA0nI7Shk4",
-  authDomain: "hgvrequest.firebaseapp.com",
-  projectId: "hgvrequest",
-  storageBucket: "hgvrequest.firebasestorage.app",
-  messagingSenderId: "382442573607",
-  appId: "1:382442573607:web:96d3303b01e3fce996e302"
-};
