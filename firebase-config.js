0ac1031212de545974738cf540a446e632cff411
igeinaoshi-prog <igeinaoshi@gmail.com>
2026-09-21 16:54:30 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,10 +6,10 @@
 // 読み込まれます(1か所で管理)。
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDdiX3qVQl4rxQPXMdin0Zt1xA0nI7Shk4",
+  authDomain: "hgvrequest.firebaseapp.com",
+  projectId: "hgvrequest",
+  storageBucket: "hgvrequest.firebasestorage.app",
+  messagingSenderId: "382442573607",
+  appId: "1:382442573607:web:96d3303b01e3fce996e302"
 };
